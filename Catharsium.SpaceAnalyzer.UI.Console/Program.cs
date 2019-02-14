f65f4bb0456b23ef402f920fa0df33956c5bb3fd
Thierry Brachthuizer <tbrachthuizer@zetsolutions.nl>
2019-02-14 15:01:02 +0100

    Create new .NET Core 2.2 project

--- Catharsium.SpaceAnalyzer.UI.Console/Program.cs
+++ Catharsium.SpaceAnalyzer.UI.Console/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.IO;
 
-namespace Catharsium.SpaceAnalyzer.UI.Console
+namespace SmooFooz.SpaceAnalyzer.Console
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var d = new DirectoryInfo("C:\\");
+            var bytes = GetFolderSize(d);
+            var kbytes = bytes/1024;
+            bytes %= 1024;
+            var mbytes = kbytes/1024;
+            kbytes %= 1024;
+            var gbytes = mbytes/1024;
+            mbytes %= 1024;
+            System.Console.WriteLine($"{gbytes} GB, {mbytes} MB, {kbytes} KB, {bytes} B");
+            System.Console.ReadLine();
+        }
+
+
+        public static long GetFolderSize(DirectoryInfo directory)
+        {
+            long result = 0;
+            foreach (var file in directory.GetFiles())
+            {
+                try
+                {
+                    result += GetFileSize(file);
+                }
+                catch(Exception ex)
+                {
+                    System.Console.WriteLine($"{ex.GetType().Name} for {file.FullName}");
+                }
+            }
+            foreach (var subDir in directory.GetDirectories())
+            {
+                try
+                {
+                    result += GetFolderSize(subDir);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine($"{ex.GetType().Name} for {subDir.FullName}");
+                }
+            }
+            return result;
+        }
+
+
+        public static long GetFileSize(FileInfo file)
+        {
+            return file.Length;
         }
     }
 }
